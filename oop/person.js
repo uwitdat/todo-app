37c0fe3d7695449cb89c5e7db58ac6990ce49f27
Ben Shekhtman <benshekhtman@hotmail.com>
2021-07-01 12:23:30 -0400

    prototype inheritance

--- oop/person.js
+++ oop/person.js
@@ -1,31 +1,26 @@
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
+//PROTOTYPE INHERITANCE
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
+        this.likes.forEach(like => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        return bio
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
-
-    this.likes.forEach(like => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-    return bio
-}
-
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
-
-const andrew = new Person('Andrew', 'Mead', 27, ['teaching', 'biking'])
-andrew.setName('Alexis Turner')
-
-
-const ben = new Person('Ben', 'Cohen', 28)
-
+const andrew = new Person('Andrew', 'Mead', 27, ['Teaching'])
+andrew.setName('Ben Johnston')
 console.log(andrew.getBio())
-console.log(ben.getBio())

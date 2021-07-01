c63f15d63cf7ec4ebd2701ff425d930dafab9f7e
Ben Shekhtman <benshekhtman@hotmail.com>
2021-07-01 02:24:26 -0400

    hangman-game

--- oop/person.js
+++ oop/person.js
@@ -1,7 +1,31 @@
-const person = function () {
+const Person = function (firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
 
 }
 
-const me = new person()
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
 
-console.log(me)
+    this.likes.forEach(like => {
+        bio += ` ${this.firstName} likes ${like}.`
+    })
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+const andrew = new Person('Andrew', 'Mead', 27, ['teaching', 'biking'])
+andrew.setName('Alexis Turner')
+
+
+const ben = new Person('Ben', 'Cohen', 28)
+
+console.log(andrew.getBio())
+console.log(ben.getBio())
